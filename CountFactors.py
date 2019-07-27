e0ca1ea4e7c85807be8990fb0e3feba0d6965cd5
sadiayousafzai036 <sadia.yousafzai93@gmail.com>
2019-07-26 17:51:13 -0700

    Add files via upload

---
+++ CountFactors.py
@@ -0,0 +1,13 @@
+def solution(N):
+    i=1
+    result=0
+    while (i*i<N):
+        if (N%i==0):
+            result+=2
+        i+=1
+    if (i*i==N):
+        result+=1
+    return result
+
+
+print(solution(1))
