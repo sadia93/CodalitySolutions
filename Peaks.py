2caf67d891b9b32251457f73933518df43d5d4c3
sadiayousafzai036 <sadia.yousafzai93@gmail.com>
2019-07-29 11:42:39 -0700

    Add files via upload

--- Peaks.py
+++ Peaks.py
@@ -1,48 +1,28 @@
-def solution(N):
-    peaks=0
-    totalpeaks=0
-    maxpeakscount=None
-    A=[]
-    if len(N)==1:
-        return 0
-    for i in range(1,len(N)-1):
-        if N[i]>N[i-1] and N[i]>N[i+1]:
-            A.append(N[i])
-            peaks+=1
-    totalpeaks=peaks
-    while peaks!=0:
-        j=0
-        i=0
-        if (len(N)%(len(N)//(peaks)))==0:
-            BlocksSize=(len(N)//(peaks))
-            peakscount=0
-            while j<len(N):
-                if A[i] in (N[j:j+BlocksSize]):
-                    peakscount+=1
-                else:
-                    break
-                i+=1
-                j+=BlocksSize
-            if maxpeakscount==None:
-                maxpeakscount=peakscount
-            else:
-                if maxpeakscount<peakscount:
-                    maxpeakscount=peakscount
-        else:
-            break
-        peaks-=1
-    if totalpeaks == 0 :
-        return 0
-    elif maxpeakscount==None:
-        return 1
-    elif totalpeaks>=maxpeakscount:
-        return maxpeakscount
-    else:
-        return 1
+def solution(A):
+  peaks = []
+  length = len(A)
+  for i in range(1, length-1):
+    if A[i-1] < A[i] > A[i+1]:
+      peaks.append(i)
 
+  count = len(peaks)
+  if count == 0:
+    return 0
 
-#(1, 2, 3, 4), (3, 4, 1, 2), (3, 4, 6, 2)
-print(solution([0,1,0,1,0,1,0,1,0,1,0]))
+  for i in range(count, 0, -1):
+    if len(A) % i == 0:
+      blockSize = length // i
+      found = [0] * i
+      foundCount = 0
+      for peak in peaks:
+        blockIndex = peak // blockSize
+        if found[blockIndex] == 0:
+          found[blockIndex] = 1
+          foundCount += 1
 
-#print(solution([1,2,3,4,3,4,1,2,3,4,6,2]))
+      if foundCount == i:
+        return i
 
+  return 0
+
+print(solution([1,2,3,4,3,4,1,2,3,4,6,2]))
